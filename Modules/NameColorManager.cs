d4aa53e4c3415a19cc310111f5019fcb105acb19
guimaem <128631050+guimaem@users.noreply.github.com>
2023-11-14 21:21:23 -0300

    fix

--- Modules/NameColorManager.cs
+++ Modules/NameColorManager.cs
@@ -137,6 +137,7 @@ public static class NameColorManager
                 if (target.Is(CustomRoles.Rascal)) color = Main.roleColors[CustomRoles.Impostor];
                 if (target.Is(CustomRoleTypes.Crewmate)) color = Main.roleColors[CustomRoles.Bait];
                 if (target.Is(CustomRoleTypes.Neutral)) color = Main.roleColors[CustomRoles.SwordsMan];
+                if (target.Is(CustomRoles.Exploiter) && seer.Is(CustomRoles.Exploiter)) color = Main.roleColors[CustomRoles.Exploiter];
                 if (target.Is(CustomRoles.Charmed)) color = Main.roleColors[CustomRoles.SwordsMan];
                 if (target.Is(CustomRoles.Infected)) color = Main.roleColors[CustomRoles.SwordsMan];
                 if (target.Is(CustomRoles.Contagious)) color = Main.roleColors[CustomRoles.SwordsMan];
