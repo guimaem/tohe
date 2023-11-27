8e418195e86567cf476b805309471f352ccf1677
guimaem <128631050+guimaem@users.noreply.github.com>
2023-11-27 18:20:37 -0300

    horse

--- Patches/MainMenuManagerPatch.cs
+++ Patches/MainMenuManagerPatch.cs
@@ -292,10 +292,10 @@ public static class MainMenuManagerPatch
 [HarmonyPatch(typeof(Constants), nameof(Constants.ShouldHorseAround))]
 public static class HorseModePatch
 {
-    public static bool isHorseMode = false;
+    //public static bool isHorseMode = false;
     public static bool Prefix(ref bool __result)
     {
-        __result = isHorseMode;
+        __result = Main.HorseMode.Value;
         return false;
     }
 }
