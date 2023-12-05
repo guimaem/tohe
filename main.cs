2d2dd0470b1e44b26e25573e69fc2c7e5819c4dd
guimaem <128631050+guimaem@users.noreply.github.com>
2023-12-05 20:39:43 -0300

    change ver

--- main.cs
+++ main.cs
@@ -41,8 +41,8 @@ public class Main : BasePlugin
     public static readonly string MainMenuText = " ";
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
-    public const string PluginVersion = "1.2.1";
-    public const string PluginDisplayVersion = "1.2.1";
+    public const string PluginVersion = "1.3.0";
+    public const string PluginDisplayVersion = "1.3.0";
     public static readonly string SupportedVersionAU = "2023.10.24";
     public const bool Canary = false; // Unused variable?
 
